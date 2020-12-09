35b04fce66c178f8dcadfe6a18baa3ebeff6bae6
alihany <alihanyiilmaz@gmail.com>
2020-12-09 21:22:24 +0300

    Add SCSS modules

---
+++ src/components/title/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './style.module.scss'
+
+const header = () => {
+  return (
+    <div className={style.header}>
+      <h1>selammm sdasdbusadrdayım</h1>
+    </div>
+  )
+}
+
+export default header
