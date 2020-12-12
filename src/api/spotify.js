de6334d8f7828fa08e9eaf7bcf569d062dea07aa
alihany <alihanyiilmaz@gmail.com>
2020-12-12 03:49:36 +0300

    Add functions to get user info and user's top tracks

--- src/api/spotify.js
+++ src/api/spotify.js
@@ -1,5 +1,8 @@
 import Spotify from 'spotify-web-api-js'
 
+const spotifyApi = new Spotify()
+let globalAccessToken = ''
+
 export function redirectUrlToSpotifyForLogin() {
   const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID
   const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI
@@ -7,6 +10,7 @@ export function redirectUrlToSpotifyForLogin() {
     'user-modify-playback-state',
     'user-library-read',
     'user-library-modify',
+    'user-top-read',
     'playlist-read-private',
     'playlist-modify-public',
     'playlist-modify-private'
@@ -27,19 +31,58 @@ export function checkUrlForSpotifyAccessToken() {
   if (!accessToken) {
     return false
   } else {
+    setAccessToken(accessToken)
     return accessToken
   }
 }
 
 function getHashParams() {
-  //helper function to parse the query string that spotify sends back when you log in
   var hashParams = {}
   var e,
     r = /([^&;=]+)=?([^&;]*)/g,
     q = window.location.hash.substring(1)
-  // eslint-disable-next-line
+  window.location.hash = ''
   while ((e = r.exec(q))) {
     hashParams[e[1]] = decodeURIComponent(e[2])
   }
   return hashParams
+}
+
+export function setAccessToken(accessToken) {
+  spotifyApi.setAccessToken(accessToken)
+  globalAccessToken = accessToken
+}
+
+export async function getMyInfo() {
+  try {
+    const personResponse = await spotifyApi.getMe()
+    const {
+      country,
+      display_name,
+      external_urls: { spotify },
+      images: [{ url }]
+    } = personResponse
+    const personInfo = {
+      country: country,
+      name: display_name,
+      link: spotify,
+      avatar: url
+    }
+    return personInfo
+  } catch (err) {
+    console.error('Error: Attempting to get user information', err)
+    console.error(err.stack)
+    return [{ id: null, playlistName: "Can't get your info!" }]
+  }
+}
+
+export async function getMyTracks() {
+  try {
+    const personResponse = await spotifyApi.getMyTopTracks({ limit: 50 })
+    console.log(personResponse)
+  } catch (err) {
+    console.error('Error: Attempting to get user information', err)
+    console.error(err.stack)
+    return [{ id: null, playlistName: "Can't get your info!" }]
+  }
 }
