e3f508f263cb06934430b1ab53395b7a867f622e
alihany <alihanyiilmaz@gmail.com>
2020-12-14 21:41:04 +0300

    Add scss variables, global styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
-import styles from './app.module'
+import styles from './styles/app.module.scss'
 import SpotifyContainer from './components/spotify-container.js'
 
 const App = () => {
-  return (
-    <>
-      <h2 className={styles.red}>This is our React application!</h2>
-      <div className="ConnectSpotify">
-        <SpotifyContainer />
-      </div>
-    </>
-  )
+  return <SpotifyContainer />
 }
 
 export default hot(module)(App)
