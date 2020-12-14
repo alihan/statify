593bf3eb3b99dcb8d823b87ec7c8f070569cd90c
alihany <alihanyiilmaz@gmail.com>
2020-12-14 21:41:47 +0300

    Add and style login component

--- src/components/connect-spotify/index.js
+++ src/components/connect-spotify/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import styles from './styles.module.scss'
 import * as SpotifyFunctions from '../../api/spotify.js'
+import Spotify from '../icons/spotify'
+import Statify from '../icons/statify'
 
 const ConnectSpotify = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <Statify />
+      <h3 className={styles.title}>Stats for Spotify</h3>
       <a href={SpotifyFunctions.redirectUrlToSpotifyForLogin()}>
-        <button>Connect to Spotify</button>
+        <button className={styles.button}>
+          Connect to Spotify
+          <Spotify />
+        </button>
       </a>
     </div>
   )
