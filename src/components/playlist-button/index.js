01d5c9213dc058be70dcf4f4c0bd81bc62632a5b
alihany <alihanyiilmaz@gmail.com>
2020-12-15 13:36:09 +0300

    Add toast

--- src/components/playlist-button/index.js
+++ src/components/playlist-button/index.js
@@ -1,21 +1,64 @@
 import React from 'react'
 import * as SpotifyFunctions from '../../api/spotify.js'
 import style from './styles.module.scss'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-const PlaylistButton = ({ id, tracks }) => {
-  async function createPlaylist(id, tracks) {
-    let playlist = await SpotifyFunctions.createPlaylist(id)
-    SpotifyFunctions.addSongsToPlaylist(playlist, tracks)
+const PlaylistButton = ({ uid, tracks, name }) => {
+  const successToast = () =>
+    toast.success('🚀 Your playlist is created!', {
+      position: 'bottom-right',
+      autoClose: 3000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      style: {
+        backgroundColor: '#1db954',
+        color: '#white',
+        fontFamily: 'Inter',
+        fontSize: '18px',
+        borderRadius: '8px'
+      }
+    })
+
+  const errorToast = () =>
+    toast.error('🥺 Playlist could not be created.', {
+      position: 'bottom-right',
+      autoClose: 3000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      style: {
+        backgroundColor: '#eb1e32',
+        color: 'white',
+        fontFamily: 'Inter',
+        fontSize: '18px',
+        borderRadius: '8px'
+      }
+    })
+
+  async function createPlaylist(uid, tracks, name) {
+    let id = await SpotifyFunctions.createPlaylist(uid, name, tracks)
+    if (id === undefined) {
+      errorToast()
+    } else {
+      successToast()
+    }
   }
 
   return (
     <div className={style.container}>
       <button
         className={style.button}
-        onClick={() => createPlaylist(id, tracks)}
+        onClick={() => createPlaylist(uid, tracks, name)}
       >
         Create Playlist
       </button>
+      <ToastContainer />
     </div>
   )
 }
